39bc4a5e6f4d513c30e6dadfacfba70d1c46c34a
Max Herich <max.herich@gmail.com>
2026-01-11 22:15:01 +0100

    Úprava třídy DatabaseConnection

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+import os
 
 class DatabaseConnection:
     __instance = None
@@ -7,7 +8,12 @@ class DatabaseConnection:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
             try:
-                with open("config.json") as json_file:
+
+                aktutalni_cesta = os.path.dirname(__file__)
+                cesta_kongiguracniho_souboru = os.path.join(aktutalni_cesta, '..', 'config', 'config.json')
+                if not os.path.exists(cesta_kongiguracniho_souboru):
+                    raise FileNotFoundError("Chyba: Konfigurační soubor config.json nebyl nalezen.")
+                with open(cesta_kongiguracniho_souboru) as json_file:
                     data = json.load(json_file)
                     cls.connection = mysql.connector.connect(
                         host=data.get("host"),
